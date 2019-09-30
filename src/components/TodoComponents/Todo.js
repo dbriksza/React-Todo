d1069490329ad3df71c0d7d9850937ecd806cf96
dbriksza <dbriksza@gmail.com>
2019-09-30 17:55:10 -0400

    project mostly working

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,71 @@
+import React from "react";
+import TodoItem from "./TodoList";
+import todosData from "./TodoData";
+// import TodoForm from "./TodoForm";
+
+class Todos extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: todosData,
+      newTodo: {
+        id: "",
+        text: "",
+        completed: false
+      }
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(id) {
+    this.setState(prevState => {
+      const updatedTodos = prevState.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      });
+      return {
+        todos: updatedTodos
+      };
+    });
+  }
+  handleChange1 = e => {
+    this.setState({
+      newTodo: {
+        ...this.state.newTodo,
+        [e.target.name]: e.target.value,
+        id: Date.now()
+      }
+    });
+  };
+  addTodo = e => {
+    e.preventDefault();
+    console.log(this.state.todos);
+    this.state.todos.push(this.state.newTodo);
+  };
+
+  render() {
+    const todoItems = this.state.todos.map(item => (
+      <TodoItem key={item.id} item={item} handleChange={this.handleChange} />
+    ));
+
+    return (
+      <div className="todo-list">
+        <form onSubmit={this.addTodo}>
+          <input
+            type="text"
+            name="text"
+            value={this.state.newTodo.text}
+            onChange={this.handleChange1}
+          />
+          <button>Add Todo!</button>
+        </form>
+        <h2 className="todos-header">Event Todos</h2>
+        {todoItems}
+      </div>
+    );
+  }
+}
+
+export default Todos;
---
+++ src/components/TodoComponents/TodoData.js
@@ -0,0 +1,34 @@
+const todosData = [
+  {
+    id: 1,
+    text: "Create event component",
+    completed: true
+  },
+  {
+    id: 2,
+    text: "Create Event Info component",
+    completed: false
+  },
+  {
+    id: 3,
+    text: "Create Event Main Section component",
+    completed: false
+  },
+  {
+    id: 4,
+    text: "Create Navigation component",
+    completed: true
+  },
+  {
+    id: 5,
+    text: "Create Footer component",
+    completed: false
+  },
+  {
+    id: 6,
+    text: "Add Styles to components",
+    completed: false
+  }
+];
+
+export default todosData;
